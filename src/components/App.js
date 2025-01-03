4d76dff4b1ba69d593c70bc8cfd2064ae2c356b9
sraj39273@gmail.com <sraj39273@gmail.com>
2025-01-03 12:12:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
 
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
 const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+    const fetchJsonData = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((dataOpr) => {
+        setIsLoading(false);
+        setData(dataOpr); 
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false); 
+      });
+  }
+
+  useEffect(()=>{
+    fetchJsonData();
+  },[])
+
+  const memoizedData = useMemo(() => data, [data]); 
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        memoizedData.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
